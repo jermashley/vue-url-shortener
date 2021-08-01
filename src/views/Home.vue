908d839789e8faf21fb6aad94aa3acd106fa4b25
Jeremiah Ashley <jeremiah.ashley@live.com>
2021-08-01 17:17:57 -0500

    Sort of inital commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <select
+      name="users"
+      id="users"
+      v-model="activeUser"
+      class="mb-12 px-2 py-1 text-base rounded-lg border border-coolGray-400"
+    >
+      <option v-for="user in users" :key="user.id" :value="user.id">
+        {{ user.name }}
+      </option>
+    </select>
+
+    <Form />
+
+    <div class="mt-12" v-if="shortenedLink.length >= 1">
+      <a :href="shortenedLink" target="_blank">{{ shortenedLink }}</a>
+    </div>
+
+    <div class="mt-12" v-if="error.length >= 1">
+      <p class="text-base text-red-700">{{ error }}</p>
+    </div>
+
+    <div class="mt-12">
+      <p v-if="filteredShortenedLinksByUser.length === 0">
+        You haven't shortened any links yet!
+      </p>
+
+      <ul v-else>
+        <li
+          v-for="link in filteredShortenedLinksByUser"
+          :key="link.id"
+          class="flex flex-row justify-start items-center gap-x-4"
+        >
+          <a :href="link.secureShortURL">{{ link.secureShortURL }}</a>
+          <button type="button" @click="deleteStoredLink(link.id)">❌</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { useUsers } from "../components/hooks/useUsers";
+import { useShortenedLinks } from "../components/hooks/useShortenedLinks";
+import Form from "../components/Form";
+import { computed } from "vue";
 
 export default {
-  name: "Home",
+  name: `Home`,
+
+  setup() {
+    const { users, activeUser, setActiveUser } = useUsers();
+
+    const { shortenedLinks, shortenedLink, error, deleteStoredLink } = useShortenedLinks();
+
+    const filteredShortenedLinksByUser = computed(() => {
+      const filteredLinks = shortenedLinks.value.filter(
+        (shortenedLink) => (shortenedLink.userId = activeUser)
+      );
+
+      return filteredLinks;
+    });
+
+    const switchUser = (id) => {
+      setActiveUser(id);
+    };
+
+    return {
+      users,
+      activeUser,
+      switchUser,
+      filteredShortenedLinksByUser,
+      shortenedLink,
+      deleteStoredLink,
+      error,
+      shortenedLinks,
+    };
+  },
+
   components: {
-    HelloWorld,
+    Form,
   },
 };
 </script>
